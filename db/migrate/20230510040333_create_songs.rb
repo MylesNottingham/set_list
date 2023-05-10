dade03e20cc33c575b7dcf4cd4791e69e9205d1a
Myles Nottingham <MylesNottingham@gmail.com>
2023-05-09 22:54:59 -0600

    Feat: Add song show.

--- db/migrate/20230510040333_create_songs.rb
+++ db/migrate/20230510040333_create_songs.rb
@@ -1,7 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[7.0]
   def change
     create_table :songs do |t|
-      t.string :name
+      t.string :title
       t.integer :length
       t.integer :play_count
 
